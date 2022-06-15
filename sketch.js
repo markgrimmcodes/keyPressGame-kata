4ebaa10996d86a79cf0a79da4a0c90bb7043ce33
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-15 16:34:35 +0200

    finished, countdown running, score increment/decrement on input letter

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+let time;
+let score;
+let inputLetter;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
+  startGame();
+}
+function startGame() {
+  inputLetter = "a";
+  score = 0;
+  time = 600;
 }
 
 function draw() {
   background(220);
+
+  text("Time:" + time, 10, 20);
+  text("Score:" + score, 10, 40);
+  text("Press " + inputLetter, 50, 100);
+
+  if (time > 0) {
+    time--;
+  } else {
+    text("GGWP! Press to start again!", 20, 120);
+  }
+}
+function mousePressed() {
+  if (time === 0) {
+    startGame();
+  }
+}
+function keyPressed() {
+  if (time <= 0) {
+    return;
+  }
+  if (key == inputLetter) {
+    score += 1;
+    inputLetter = random("abcdefghijklmnopqrstuvwxyz".split(""));
+  } else {
+    score -= 1;
+  }
 }
